d332bc6ac1bd8b3951b6c115bfa72bd16a9d780d
yopiyama <yoshiyama.r24@gmail.com>
2020-07-29 01:57:32 +0900

    Update keymap
    親指周りのキーアサインを変更。
    1.5U 以外でもタップ/ホールドで処理を変えるものを追加した。
    NUM レイヤーにカンマとピリオドの追加、演算子類を右手移動。

--- keymap.c
+++ keymap.c
@@ -36,7 +36,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_MINS,\
     KC_LCTL,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,\
     KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_EQL,\
-                                    KC_LGUI, KC_LALT,  LT(_SYM, KC_SPC),   LT(_NUM, KC_ENT),  KC_BSPC, KC_ESC \
+                            KC_LGUI, LALT_T(KC_ESC),  LT(_SYM, KC_SPC),   LT(_NUM, KC_ENT),  LSFT_T(KC_BSPC), KC_ESC \
   ),
 
   [_LEDS] = LAYOUT( \
@@ -47,9 +47,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [_NUM] = LAYOUT( \
-    _______,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,\
+    _______,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                       KC_PPLS, KC_PMNS, KC_PAST, KC_PSLS, KC_PEQL,  KC_ENT,\
     _______,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,                       KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, XXXXXXX, XXXXXXX,\
-    _______, KC_PPLS, KC_PMNS, KC_PAST, KC_PSLS, KC_PEQL,                        KC_DEL, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,\
+    _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                       KC_DEL,  XXXXXXX, KC_COMM,  KC_DOT, XXXXXXX, XXXXXXX,\
                                         _______, _______, OSL(_LEDS),  _______, _______, _______ \
   ),
 
